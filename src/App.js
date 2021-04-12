0de6a718e4a3cef05bb0725b6569d96d3aef9442
lucasbarroso23 <lucasbarroso23@gmail.com>
2021-04-12 10:32:46 -0300

    global styles setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GloblaStyle from './styles/global';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GloblaStyle />
     </BrowserRouter>
   );
 }
