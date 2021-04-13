c0569638d5646bc63f747dc3aab1aed1cc6e739a
lucasbarroso23 <lucasbarroso23@gmail.com>
2021-04-13 11:15:59 -0300

    dispathing action

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../utils/format';
 import api from '../../services/api';
 
 import { Productlist } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -21,6 +22,15 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProject = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
+  }
+
   render() {
     const { products } = this.state;
     return (
@@ -34,7 +44,7 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button type="button" onClick={() => this.handleAddProject(product)}>
               <div>
                 <MdAddShoppingCart size="16" color="#fff" /> 3
               </div>
@@ -47,3 +57,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
